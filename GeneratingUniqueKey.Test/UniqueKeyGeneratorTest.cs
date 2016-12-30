e5ae62bf65219b9d749190ec01e1a82752eda039
Wirachai <wirachai.me@gmail.com>
2016-12-30 15:23:51 +0700

    Init project

---
+++ GeneratingUniqueKey.Test/UniqueKeyGeneratorTest.cs
@@ -0,0 +1,42 @@
+﻿
+
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GeneratingUniqueKey.Test
+{
+    [TestFixture]
+    public class UniqueKeyGeneratorTest
+    {
+        [Test]
+        public void Get_ShouldReturn_ValidLength()
+        {
+            var generator = new UniqueKeyGenerator();
+            var result = generator.Get(8);
+            Assert.AreEqual(8, result.Length);
+
+            result = generator.Get(20);
+            Assert.AreEqual(20, result.Length);
+
+            result = generator.Get(100);
+            Assert.AreEqual(100, result.Length);
+        }
+
+        [Test]
+        public void Get_ShouldReturn_UniqueValues()
+        {
+            //int round = 10000;
+            int round = 1000000;    // million
+            int size = 20;
+            var generator = new UniqueKeyGenerator();
+            var uniqueKeys = new List<string>();
+            for (int i = 0; i < round; i++)
+            {
+                var result = generator.Get(size);
+                Assert.IsFalse(uniqueKeys.Contains(result));
+
+                uniqueKeys.Add(result);
+            }
+        }
+    }
+}
---
+++ GeneratingUniqueKey/UniqueKeyGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace GeneratingUniqueKey
+{
+    public class UniqueKeyGenerator
+    {
+        private RNGCryptoServiceProvider randomer;
+        private readonly string characters;
+
+        public UniqueKeyGenerator() : this("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+        {
+
+        }
+
+        public UniqueKeyGenerator(string characters)
+        {
+            this.characters = characters;
+            this.randomer = new RNGCryptoServiceProvider();
+        }
+
+        public string Get(int size)
+        {
+            char[] chars = characters.ToCharArray();
+            byte[] data = new byte[size];
+            randomer.GetNonZeroBytes(data);
+            StringBuilder result = new StringBuilder(size);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length - 1)]);
+            }
+            return result.ToString();
+        }
+    }
+}
